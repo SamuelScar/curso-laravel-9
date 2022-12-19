9018a71f09e0edc808bba2622d1fe6c954d5a15b
Samuel Almeida <souzacarvalhosamuel@gmail.com>
2022-12-19 07:15:34 -0300

    - Criado CRUD de usuarios

---
+++ resources/views/users/create.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('title', 'Novo usuário')
+
+@section('content')
+    
+<h1>Novo usuário</h1>
+
+@include('includes.validations-form')
+
+<form
+    action="{{ route("users.store") }}"
+    method="POST"
+>
+    @include('users._partials.form');
+</form>
+
+@endsection
+
---
+++ resources/views/users/edit.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('title', "Editar o usuário {{ $user->name }}")
+
+@section('content')
+    
+<h1>Editar o usuário {{ $user->name }}</h1>
+
+@include('includes.validations-form')
+
+<form
+    action="{{ route("users.update", $user->id) }}"
+    method="POST"
+>
+    @method("PUT")
+    @include('users._partials.form');
+</form>
+
+@endsection
+
